8dfb775f79fbb79da249f9ee9728ebacfd83fefe
Blake Spangenberg <blake.spangenberg1@gmail.com>
2019-09-09 18:23:03 -0700

    start/stop/restart server from cmd line

--- src/aternos-automation.py
+++ src/aternos-automation.py
@@ -2,12 +2,32 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Constants #
+
+CHROME_PATH        = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+CHROME_DRIVER_PATH = "D:/Libraries/git/aternos-automation/bin/chromedriver"
+
+# Global Variables #
+try:
+    del driver
+    del chrome_options
+except Exception:
+    pass
+#end tryexcept
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = CHROME_PATH
+driver = webdriver.Chrome(CHROME_DRIVER_PATH,options=chrome_options)
+
+# Methods #
 
 def main():
     if(len(sys.argv) != 2):
         print("ERROR: Invalid Arguments")
         return
     #endif
+
+    login()
 
     if(sys.argv[1] == "start"):
         start()
@@ -18,40 +38,64 @@ def main():
     else:
         print("ERROR: Invalid Arguments")
     #endif
+
+    input("")
+    stop_driver()
 #end main()
 
 def start():
     global driver
-
     print("starting server...")
-    connect()
-    disconnect()
+
+    result = driver.execute_script("start();")
+    print(str(result))
 #end start()
 
 def stop():
+    global driver
     print("stopping server...")
 
+    result = driver.execute_script("stop();")
+    print(str(result))
 #end start()
 
 def restart():
+    global driver
     print("restarting server...")
     
+    result = driver.execute_script("restart();")
+    print(str(result))
 #end start()
 
-def connect():
-    global driver
-    driver = webdriver.Chrome()
+def login():
+    # attempt to open server page #
     driver.get("https://aternos.org/server/")
 
-    if("Login" in driver.title):
-        print("signing in...")
-
+    if(not ("Login" in driver.title)):
+        return # already signed in
     #endif
 
-#end connect()
+    print("signing in...")
 
-def disconnect():
+    # set username #
+    result = driver.execute_script("$(\"#user\").val(\"" + USERNAME + "\");")
+    print(str(result))
+
+    # set password #
+    result = driver.execute_script("$(\"#password\").val(\"" + PASSWORD + "\");")
+    print(str(result))
+
+    # login #
+    result = driver.execute_script("login();")
+    print(str(result))
+
+    # accept EULA #
+    result = driver.execute_script("acceptEULA();")
+    print(str(result))
+#end login()
+
+def stop_driver():
     driver.close()
-#end disconnect()
+#end stop_driver()
 
 if __name__ == "__main__": main() #don't run on import
