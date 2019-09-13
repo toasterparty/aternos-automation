1dbeecc36f058dd7d5d52b63ddf605449f706918
Blake Spangenberg <blake.spangenberg1@gmail.com>
2019-09-12 22:51:13 -0700

    removed reference to display driver

--- main.py
+++ main.py
@@ -134,15 +134,11 @@ def get_status():
 def login():
     global driver
 
-    display = Display(visible=0, size=(1024, 768))
-    display.start()
-
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(CHROME_DRIVER_PATH,options=chrome_options)
-    #driver = webdriver.Firefox()
 
     # attempt to open server page #
     driver.get("https://aternos.org/friends/")
